db615bdf87cd7ca6d6f6e9024b6b94eeed70de7b
Serik <s.akimgereyev@gmail.com>
2024-06-09 22:43:53 +0500

    add auto deployment

--- cdk_deployment/lib/cdk_deployment-stack.ts
+++ cdk_deployment/lib/cdk_deployment-stack.ts
@@ -4,42 +4,57 @@ import {
   aws_s3_deployment as s3deploy,
   aws_cloudfront as cloudfront,
   aws_cloudfront_origins as origins,
+  aws_iam as iam,
 } from "aws-cdk-lib";
-import { BlockPublicAccess } from "aws-cdk-lib/aws-s3";
 
 export class CdkDeploymentStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create an S3 bucket to host the React app
     const shopAutoBucket = new s3.Bucket(this, "ShopAutoBucket", {
       websiteIndexDocument: "index.html",
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ACLS,
       publicReadAccess: true,
-      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
-    // Create a CloudFront distribution
+    const oai = new cloudfront.OriginAccessIdentity(this, "OAI");
+
     const distribution = new cloudfront.Distribution(
       this,
       "ShopAutoDistribution",
       {
-        defaultBehavior: { origin: new origins.S3Origin(shopAutoBucket) },
+        defaultBehavior: {
+          origin: new origins.S3Origin(shopAutoBucket, {
+            originAccessIdentity: oai,
+          }),
+        },
         defaultRootObject: "index.html",
       }
     );
 
-    // Deploy the built React app to the S3 bucket
+    shopAutoBucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        actions: ["s3:GetObject"],
+        principals: [new iam.ServicePrincipal("cloudfront.amazonaws.com")],
+        resources: [shopAutoBucket.arnForObjects("*")],
+        conditions: {
+          StringEquals: {
+            "AWS:SourceArn": `arn:aws:cloudfront::${this.account}:distribution/${distribution.distributionId}`,
+          },
+        },
+      })
+    );
+
+    shopAutoBucket.grantRead(oai);
+
     new s3deploy.BucketDeployment(this, "DeployShopAuto", {
       sources: [s3deploy.Source.asset("../dist")],
       destinationBucket: shopAutoBucket,
       distribution,
-      distributionPaths: ["/*"], // Invalidate all files
+      distributionPaths: ["/*"],
     });
 
-    new cdk.CfnOutput(this, "BucketURL", {
-      value: shopAutoBucket.bucketWebsiteUrl,
-    });
     new cdk.CfnOutput(this, "DistributionDomainName", {
       value: distribution.domainName,
     });
