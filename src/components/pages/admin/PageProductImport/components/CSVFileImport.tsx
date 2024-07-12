be684e3211752bbb20ac06cfc8a189fe992787ba
Serik <s.akimgereyev@gmail.com>
2024-07-12 15:40:31 +0500

    add alerts for 401 403 errors

--- src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
+++ src/components/pages/admin/PageProductImport/components/CSVFileImport.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import axios from "axios";
+import axios from "../../../../../utils/axiosConfig";
 
 type CSVFileImportProps = {
   url: string;
@@ -42,28 +42,35 @@ export default function CSVFileImport({
 
     console.log("...getting signed URL from:", url);
 
-    // Get the presigned URL
-    const response = await axios({
-      method: "GET",
-      url,
-      params: {
-        name: encodeURIComponent(file.name),
-      },
-      headers: {
-        Authorization: `Basic ${authorizationToken}`,
-      },
-    });
+    try {
+      // Get the presigned URL
+      const response = await axios({
+        method: "GET",
+        url,
+        params: {
+          name: encodeURIComponent(file.name),
+        },
+        headers: {
+          Authorization: `Basic ${authorizationToken}`,
+        },
+      });
 
-    const { signedUrl } = response.data;
+      const { signedUrl } = response.data;
 
-    console.log("File to upload: ", file.name);
-    console.log("Uploading to: ", signedUrl);
-    const result = await fetch(signedUrl, {
-      method: "PUT",
-      body: file,
-    });
-    console.log("Result: ", result);
-    setFile(undefined);
+      console.log("File to upload: ", file.name);
+      console.log("Uploading to: ", signedUrl);
+
+      const result = await axios.put(signedUrl, file, {
+        headers: {
+          "Content-Type": file.type,
+        },
+      });
+
+      console.log("Result: ", result);
+      setFile(undefined);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    }
   };
   return (
     <Box>
